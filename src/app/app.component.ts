ad23b41e48343e2ba1e9dc4f1b7aa98b3f24ccc6
Vail Algatt <vail.algatt@gmail.com>
2017-08-17 20:28:33 -0600

    Added angular FormsModule, some styling, and displaying hard coded 'person' data to the DOM :smile:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+export class Person {
+  id: number;
+  name: string;
+  phone: number;
+  email: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{person.name}} details!</h2>
+  <div><label>id: </label>{{person.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="person.name" placeholder="name">
+  </div>
+  <div><label>Phone: </label>{{person.phone}}</div>  
+  <div>
+    <label>email: </label>
+    <input [(ngModel)]="person.email" placeholder="email@gmail.com">
+  </div>
+  
+  `
 })
+
+
 export class AppComponent {
-  title = 'app';
+  title = 'Contacts';
+  person: Person = {
+    id: 1,
+    name: 'Adam',
+    email: 'adam@gmail.com',
+    phone: 222222222
+  }
 }
+
+
+
