170f369e8a6782141db20a2e9945f3b7349dd313
Vail Algatt <vail.algatt@gmail.com>
2017-08-20 18:58:18 -0600

    displaying all data in Contacts array to the selected contacts details!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,22 @@ import { Component } from '@angular/core';
 export class Contact {
   id: number;
   name: string;
-  // phone: number;
-  // email: string;
+  phone: number;
+  email: string;
 }
 
 const CONTACTS: Contact[] = [
-  { id: 1, name: 'Adam' },
-  { id: 2, name: 'Katy' },
-  { id: 3, name: 'Kyra' },
-  { id: 4, name: 'Jill' },
-  { id: 5, name: 'Yadira' },
-  { id: 6, name: 'Judy' },
-  { id: 7, name: 'Shay' },
-  { id: 8, name: 'Bob' },
-  { id: 9, name: 'Gene' },
-  { id: 20, name: 'Robert' },
-  { id: 10, name: 'Chuy' }
+  { id: 1, name: 'Adam', phone: 111111111, email: 'adam@adam.com' },
+  { id: 2, name: 'Katy', phone: 222222222, email: 'katy@katy.com' },
+  { id: 3, name: 'Kyra', phone: 333333333, email: 'kyra@kyra.com' },
+  { id: 4, name: 'Jill', phone: 444444444, email: 'jill@jill.com' },
+  { id: 5, name: 'Yadira', phone: 555555555, email: 'yadira@yadira.com' },
+  { id: 6, name: 'Judy', phone: 666666666, email: 'judy@judy.com' },
+  { id: 7, name: 'Shay', phone: 777777777, email: 'shay@shay.com' },
+  { id: 8, name: 'Bob', phone: 888888888, email: 'bob@bob.com' },
+  { id: 9, name: 'Gene', phone: 999999999, email: 'gene@gene.com' },
+  { id: 20, name: 'Robert', phone: 123123123, email: 'robert@robert.com' },
+  { id: 10, name: 'Chuy', phone: 234234234, email: 'chuy@chuy.com' }
 ];
 
 @Component({
@@ -38,6 +38,8 @@ const CONTACTS: Contact[] = [
   <div *ngIf="selectedContact">
     <h2>{{selectedContact.name}} details!</h2>
     <div><label>id: </label>{{selectedContact.id}}</div>
+    <div><label>phone: </label>{{selectedContact.phone}}</div>
+    <div><label>email: </label>{{selectedContact.email}}</div>    
     <div>
       <label>name: </label>
       <input [(ngModel)]="selectedContact.name" placeholder="name"/>
