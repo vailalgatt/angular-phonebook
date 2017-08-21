37624a97eae0c6aac00fda9bdc78cf37d5bcd0f1
Vail Algatt <vail.algatt@gmail.com>
2017-08-21 16:26:19 -0600

    added a 'mock contacts' file & added a service file for the contact page yet (service file is still bare bones, just the set up & nothing else)

---
+++ src/app/mock-contacts.ts
@@ -0,0 +1,14 @@
+import { Contact } from './contact';
+
+export const CONTACTS: Contact[] = [
+  { id: 11, name: 'Steph', phone: 111111111, email: 'steph@steph.com' },
+  { id: 12, name: 'Lu', phone: 222222222, email: 'lu@lu.com' },
+  { id: 13, name: 'Ingrid', phone: 333333333, email: 'ingrid@ingrid.com' },
+  { id: 14, name: 'Gabby', phone: 444444444, email: 'gabby@gabby.com' },
+  { id: 15, name: 'Chippy', phone: 555555555, email: 'chippy@chippy.com' },
+  { id: 16, name: 'Jo', phone: 666666666, email: 'jo@jo.com' },
+  { id: 17, name: 'Chewy', phone: 777777777, email: 'chewy@chewy.com' },
+  { id: 18, name: 'Will', phone: 888888888, email: 'will@will.com' },
+  { id: 19, name: 'Duane', phone: 999999999, email: 'duane@duane.com' },
+  { id: 20, name: 'Jay', phone: 123123123, email: 'jay@jay.com' },
+];
